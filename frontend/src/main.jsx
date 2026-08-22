fbd4a38f2a139978eb228b4ad93d7c75ab2bfeb8
mussayib87 <mussayibibrahim@gmail.com>
2026-08-23 01:02:03 +0530

    Refactor main.jsx to use App component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,62 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
+import App from "./App";
 import "./index.css";
-
-function Home() {
-  return (
-    <div
-      style={{
-        minHeight: "100vh",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        background: "#ffffff",
-        color: "#111827",
-        fontSize: "32px",
-      }}
-    >
-      VIDYA AI HOME
-    </div>
-  );
-}
-
-function Login() {
-  return (
-    <div
-      style={{
-        minHeight: "100vh",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        background: "#ffffff",
-        color: "#111827",
-        fontSize: "32px",
-      }}
-    >
-      VIDYA AI LOGIN
-    </div>
-  );
-}
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
