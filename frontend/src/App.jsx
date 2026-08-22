c8b6d0f2722486cb338837c7c2fc14d4f83afae5
mussayib87 <mussayibibrahim@gmail.com>
2026-08-23 00:33:13 +0530

    Refactor App component structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-  import { Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
@@ -24,4 +24,4 @@ function App() {
   );
 }
 
-export default App; 
+export default App;
