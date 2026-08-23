7700d80c67b1da1cba71d8e14937cc1c6a5f3bf4
mussayib87 <mussayibibrahim@gmail.com>
2026-08-23 19:49:37 +0530

    Import vidya-design-system.css in main.jsx
    Added import for vidya-design-system.css to main.jsx.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
+import "./styles/vidya-design-system.css";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
